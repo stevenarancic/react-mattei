5ae5476ee625ca1312e84f8e8ba3f00fccaeaed4
Steve Narancic <steve.mello@narancic.com.br>
2022-02-25 13:30:29 -0300

    Update reviews-block.js
    Adição de um botão de avaliação da empresa e um título para o bloco de avaliação.

--- src/components/reviews-block.js
+++ src/components/reviews-block.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import ReviewItem from "./review-item";
+import PrimaryButton from "./primary-button";
+import OutlinedButton from "./outlined-button";
 
 export default function ReviewsBlock() {
     return (
         <Container>
+            <h4 className="text-center mt-4">Confira nossas avaliações</h4>
             <Row className="mt-4">
                 <Col sm={12} md={4} lg={4}>
                     <ReviewItem
@@ -28,6 +31,15 @@ export default function ReviewsBlock() {
                     />
                 </Col>
             </Row>
+            <div className="d-flex flex-row justify-content-center mt-3">
+                <PrimaryButton
+                    title="Avalie-nos"
+                    onClick={() =>
+                        (window.location.href =
+                            "https://g.page/r/CaZkQTHB_X1HEAg/review")
+                    }
+                />
+            </div>
         </Container>
     );
 }
