2732197fc073c4fcdffc8d544d2f89293fc6c798
Steve Narancic <steve.mello@narancic.com.br>
2022-03-03 13:33:10 -0300

    refatoração de código
    componentes-block renderizando filhos usando map.

--- src/components/reviews-block.js
+++ src/components/reviews-block.js
@@ -4,31 +4,38 @@ import ReviewItem from "./review-item";
 import PrimaryButton from "./primary-button";
 
 export default function ReviewsBlock() {
+    const data = [
+        {
+            title: "Odegar",
+            numberOfStars: 4,
+            content: "Bom atendimento e bastante variedade de produtos.",
+        },
+        {
+            title: "Agnes",
+            numberOfStars: 5,
+            content:
+                "Ótima loja de materiais elétricos e bom atendimento recomendo.",
+        },
+        {
+            title: "Jefferson",
+            numberOfStars: 4,
+            content: "Otimo atendimento. Muitas variedades.",
+        },
+    ];
+
     return (
         <Container>
             <h4 className="text-center mt-4">Confira nossas avaliações</h4>
             <Row className="mt-4">
-                <Col sm={12} md={4} lg={4}>
-                    <ReviewItem
-                        title="Odegar"
-                        numberOfStars={4}
-                        content="Bom atendimento e bastante variedade de produtos"
-                    />
-                </Col>
-                <Col sm={12} md={4} lg={4}>
-                    <ReviewItem
-                        title="Agnes"
-                        numberOfStars={5}
-                        content="Ótima loja de materiais elétricos e bom atendimento recomendo"
-                    />
-                </Col>
-                <Col sm={12} md={4} lg={4}>
-                    <ReviewItem
-                        title="Jefferson"
-                        numberOfStars={4}
-                        content="Otimo atendimento. Muitas variedades."
-                    />
-                </Col>
+                {data.map((item) => (
+                    <Col sm={12} md={4} lg={4}>
+                        <ReviewItem
+                            title={item.title}
+                            numberOfStars={item.numberOfStars}
+                            content={item.content}
+                        />
+                    </Col>
+                ))}
             </Row>
             <div className="d-flex flex-row justify-content-center mt-3">
                 <PrimaryButton
--- src/components/social-media-block.js
+++ src/components/social-media-block.js
@@ -4,29 +4,42 @@ import SocialMediaItem from "./social-media-item";
 import "../styles/social-media-block.css";
 
 export default function SocialMediaBlock() {
+    const data = [
+        {
+            icon: Facebook,
+            title: "Facebook",
+            link: "https://www.facebook.com/pages/Mattei-Materiais-El%C3%A9tricos/402893023398855",
+            color: "#1B74E4",
+        },
+        {
+            icon: Instagram,
+            title: "Instagram",
+            link: "https://www.instagram.com/matteimateriaiseletricos/",
+            color: "#BD2562",
+        },
+        {
+            icon: Whatsapp,
+            title: "Whatsapp",
+            link: "https://www.facebook.com/pages/Mattei-Materiais-El%C3%A9tricos/402893023398855",
+            color: "#1ea852",
+        },
+    ];
+
     return (
         <div className="mt-1">
             {/* <h5>Redes Sociais</h5> */}
             <div className="social-media-block">
-                <SocialMediaItem
-                    icon={<Facebook />}
-                    title="Facebook"
-                    link="https://www.facebook.com/pages/Mattei-Materiais-El%C3%A9tricos/402893023398855"
-                    color="#1B74E4"
-                />
-                <div className="ms-3" />
-                <SocialMediaItem
-                    icon={<Instagram />}
-                    title="Instagram"
-                    link="https://www.instagram.com/matteimateriaiseletricos/"
-                    color="#BD2562"
-                />
-                <div className="ms-3" />
-                <SocialMediaItem
-                    icon={<Whatsapp />}
-                    title="Whatsapp"
-                    color="#1ea852"
-                />
+                {data.map((item) => (
+                    <>
+                        <SocialMediaItem
+                            icon={<item.icon />}
+                            title={item.title}
+                            link={item.link}
+                            color={item.color}
+                        />
+                        <div className="me-3" />
+                    </>
+                ))}
             </div>
         </div>
     );
