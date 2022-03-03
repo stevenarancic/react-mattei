d035e3e33e86432df2f1b0502955451b7567a31a
Steve Narancic <steve.mello@narancic.com.br>
2022-03-03 12:25:27 -0300

    refatoração
    refatoração do componente contact-block, utilizando arrays e JSON.

--- src/components/contact-block.js
+++ src/components/contact-block.js
@@ -12,64 +12,61 @@ import ContactCard from "./contact-card";
 import OutlinedButton from "./outlined-button";
 
 export default function ContactBlock() {
+    const data = [
+        {
+            icon: Phone,
+            iconFill: PhoneFill,
+            title: "Telefones",
+            content: [
+                "(51) 3033-5050",
+                "(51) 3473-3115",
+                <a href="https://api.whatsapp.com/send?phone=555199964-0860">
+                    (51) 99964-0860
+                </a>,
+            ],
+        },
+        {
+            icon: Envelope,
+            iconFill: EnvelopeFill,
+            title: "E-Mail",
+            content: [
+                <a href="mailto: mattei@mattei.com.br">mattei@mattei.com.br</a>,
+                <a href="mailto: financeiro@mattei.com.br">
+                    financeiro@mattei.com.br
+                </a>,
+            ],
+        },
+        {
+            icon: GeoAlt,
+            iconFill: GeoAltFill,
+            title: "Endereço",
+            content: [
+                "Rua 24 de Agosto, 2756. Esteio / RS",
+                <OutlinedButton title="Como Chegar" />,
+            ],
+        },
+    ];
+
     return (
         <div>
             <h5>Contato</h5>
             <Row>
-                <Col sm={12} md={4} lg={4}>
-                    <ContactCard
-                        icon={<Phone />}
-                        iconFill={<PhoneFill />}
-                        title="Telefones"
-                        content={
-                            <ul style={{ listStyle: "none" }}>
-                                <li>(51) 3033-5050</li>
-                                <li>(51) 3473-3115</li>
-                                <li>
-                                    <a href="https://api.whatsapp.com/send?phone=555199964-0860">
-                                        (51) 9 9964-0860
-                                    </a>
-                                </li>
-                            </ul>
-                        }
-                    />
-                </Col>
-                <Col sm={12} md={4} lg={4}>
-                    <ContactCard
-                        icon={<Envelope />}
-                        iconFill={<EnvelopeFill />}
-                        title="E-Mail"
-                        content={
-                            <ul style={{ listStyle: "none" }}>
-                                <li>
-                                    <a href="mailto: mattei@mattei.com.br">
-                                        mattei@mattei.com.br
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="mailto: financeiro@mattei.com.br">
-                                        financeiro@mattei.com.br
-                                    </a>
-                                </li>
-                            </ul>
-                        }
-                    />
-                </Col>
-                <Col sm={12} md={4} lg={4}>
-                    <ContactCard
-                        icon={<GeoAlt />}
-                        iconFill={<GeoAltFill />}
-                        title="Endereço"
-                        content={
-                            <ul style={{ listStyle: "none" }}>
-                                <li>Rua 24 de Agosto, 2756. Esteio / RS</li>
-                                <li className="mt-2">
-                                    <OutlinedButton title="Como Chegar" />
-                                </li>
-                            </ul>
-                        }
-                    />
-                </Col>
+                {data.map((item) => (
+                    <Col>
+                        <ContactCard
+                            title={item.title}
+                            icon={<item.icon />}
+                            iconFill={<item.iconFill />}
+                            content={
+                                <ul style={{ listStyle: "none" }}>
+                                    {item.content.map((content) => (
+                                        <li>{content}</li>
+                                    ))}
+                                </ul>
+                            }
+                        />
+                    </Col>
+                ))}
             </Row>
         </div>
     );
