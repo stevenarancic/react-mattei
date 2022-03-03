f950ed32e7e6ff2e0898599b050016f8d646cbce
Steve Narancic <steve.mello@narancic.com.br>
2022-03-03 13:48:53 -0300

    refatoração de código
    mais blocks usando .map.

--- src/components/company-providers-block.js
+++ src/components/company-providers-block.js
@@ -3,6 +3,13 @@ import { Container } from "react-bootstrap";
 import "../styles/company-providers-block.css";
 
 export default function CompanyProvidersBlock() {
+    const data = [
+        "https://mattei.com.br/images/marcas/113_53248Cemar_logo.png",
+        "https://mattei.com.br/images/marcas/110_86016tramontina-logo.png",
+        "https://mattei.com.br/images/marcas/111_20480Schneider_Electric.svg_.png",
+        "https://mattei.com.br/images/marcas/112_0ourolux-201507062305561.png",
+    ];
+
     return (
         <Container>
             <div className="card rounded bg-white mt-3 pb-4">
@@ -11,78 +18,13 @@ export default function CompanyProvidersBlock() {
                 </h2>
                 <div className="slider">
                     <div className="slider-track">
-                        <div className="slide">
-                            <img
-                                src="https://mattei.com.br/images/marcas/113_53248Cemar_logo.png"
-                                alt=""
-                            />
-                        </div>
-                        <div className="slide">
-                            <img
-                                src="https://mattei.com.br/images/marcas/110_86016tramontina-logo.png"
-                                alt=""
-                            />
-                        </div>
-                        <div className="slide">
-                            <img
-                                src="https://mattei.com.br/images/marcas/111_20480Schneider_Electric.svg_.png"
-                                alt=""
-                            />
-                        </div>
-                        <div className="slide">
-                            <img
-                                src="https://mattei.com.br/images/marcas/112_0ourolux-201507062305561.png"
-                                alt=""
-                            />
-                        </div>
-                        <div className="slide">
-                            <img
-                                src="https://mattei.com.br/images/marcas/113_53248Cemar_logo.png"
-                                alt=""
-                            />
-                        </div>
-                        <div className="slide">
-                            <img
-                                src="https://mattei.com.br/images/marcas/110_86016tramontina-logo.png"
-                                alt=""
-                            />
-                        </div>
-                        <div className="slide">
-                            <img
-                                src="https://mattei.com.br/images/marcas/111_20480Schneider_Electric.svg_.png"
-                                alt=""
-                            />
-                        </div>
-                        <div className="slide">
-                            <img
-                                src="https://mattei.com.br/images/marcas/112_0ourolux-201507062305561.png"
-                                alt=""
-                            />
-                        </div>{" "}
-                        <div className="slide">
-                            <img
-                                src="https://mattei.com.br/images/marcas/113_53248Cemar_logo.png"
-                                alt=""
-                            />
-                        </div>
-                        <div className="slide">
-                            <img
-                                src="https://mattei.com.br/images/marcas/110_86016tramontina-logo.png"
-                                alt=""
-                            />
-                        </div>
-                        <div className="slide">
-                            <img
-                                src="https://mattei.com.br/images/marcas/111_20480Schneider_Electric.svg_.png"
-                                alt=""
-                            />
-                        </div>
-                        <div className="slide">
-                            <img
-                                src="https://mattei.com.br/images/marcas/112_0ourolux-201507062305561.png"
-                                alt=""
-                            />
-                        </div>
+                        {[...Array(3)].map(() =>
+                            data.map((item) => (
+                                <div className="slide">
+                                    <img src={item} alt="" />
+                                </div>
+                            ))
+                        )}
                     </div>
                 </div>
             </div>
--- src/components/payment-methods-block.js
+++ src/components/payment-methods-block.js
@@ -1,46 +1,42 @@
 import React from "react";
 
 export default function PaymentsMethodBlock() {
+    const data = [
+        {
+            image: "https://mattei.com.br/images/cartoes/24_102400boleto.jpg",
+            alt: "Boleto",
+        },
+        {
+            image: "https://mattei.com.br/images/cartoes/5.png",
+            alt: "Mastercard",
+        },
+        {
+            image: "https://mattei.com.br/images/cartoes/4.png",
+            alt: "Hipercard",
+        },
+        {
+            image: "https://mattei.com.br/images/cartoes/3.png",
+            alt: "elo",
+        },
+        {
+            image: "https://mattei.com.br/images/cartoes/2.png",
+            alt: "Amex",
+        },
+        {
+            image: "https://mattei.com.br/images/cartoes/1.png",
+            alt: "VISA",
+        },
+    ];
+
     return (
         <div className="mt-4 mb-4">
             <h5>Métodos de Pagamento</h5>
             <div class="d-flex flex-row">
-                <div class="me-1">
-                    <img
-                        src="https://mattei.com.br/images/cartoes/24_102400boleto.jpg"
-                        alt="Boleto"
-                    />
-                </div>
-                <div class="me-1">
-                    <img
-                        src="https://mattei.com.br/images/cartoes/5.png"
-                        alt="Mastercard box"
-                    />
-                </div>
-                <div class="me-1">
-                    <img
-                        src="https://mattei.com.br/images/cartoes/4.png"
-                        alt="Hipercard box"
-                    />
-                </div>
-                <div class="me-1">
-                    <img
-                        src="https://mattei.com.br/images/cartoes/3.png"
-                        alt="elo"
-                    />
-                </div>
-                <div class="me-1">
-                    <img
-                        src="https://mattei.com.br/images/cartoes/2.png"
-                        alt="Amex"
-                    />
-                </div>
-                <div class="me-1">
-                    <img
-                        src="https://mattei.com.br/images/cartoes/1.png"
-                        alt="VISA"
-                    />
-                </div>
+                {data.map((item) => (
+                    <div className="me-1">
+                        <img src={item.image} alt={item.alt} />
+                    </div>
+                ))}
             </div>
         </div>
     );
